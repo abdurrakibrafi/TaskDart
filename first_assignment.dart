8ecea25e0bebd3c20f2037c3dd36685440b32715
Abdur Rakib Rafi <86319780+abdurrakibrafi@users.noreply.github.com>
2023-08-02 20:58:54 +0600

    Add files via upload
    task code pushed

---
+++ first_assignment.dart
@@ -0,0 +1,25 @@
+void main() {
+    const int a = 7; //declare with constant value
+    const int b = 3; //declare with constant value
+
+    // for Addition
+    int _addition = a + b;
+    //using string interpulation we print the message in console
+    print('Addition: $a + $b = $_addition');
+
+    // for Subtraction
+    int _subtraction = a - b;
+    print('Subtraction: $a - $b = $_subtraction');
+
+    // for Multiplication
+    int _multiplication = a * b;
+    print('Multiplication: $a * $b = $_multiplication');
+
+    // for division
+    double _division = a / b;
+    print('Division: $a / $b = $_division');
+
+    // for Modulus
+    int _modulus = a % b;
+    print('Modulus: $a % $b = $_modulus');
+  }
