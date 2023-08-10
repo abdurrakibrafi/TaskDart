a65a9bca88fca8aed28fff346944c04cbf1f2823
Abdur Rakib Rafi <86319780+abdurrakibrafi@users.noreply.github.com>
2023-08-10 20:52:52 +0600

    Add files via upload

---
+++ second_assignment.dart
@@ -0,0 +1,24 @@
+void main() {
+  List<int> studentsGrade = [85, 92, 78, 65, 88, 72];
+
+  double sum = 0.0; 
+  //for-in loop apply for iterate the grade of students and addition the total grade.
+  for (var grade in studentsGrade) {
+     sum = sum + grade;
+  }
+
+  double average = sum / studentsGrade.length; //apply the formula(sum Of Grade/the List of lengtg).
+  int averageResult=average.toInt(); //convert double to int using toInt().
+  int roundedAverage = average.round(); //here round the grade.
+
+  print("Student's average grade: $averageResult");
+  print("Rounded average: $roundedAverage");
+
+  if (roundedAverage >= 70) {
+    print("Passed");
+  } else {
+    print("Failed");
+  }
+}
+
+
